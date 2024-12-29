aebe18dbe22ab14d4d744f1722b05ee64824d10f
Rikuo <plumbook.menchan@gmail.com>
2024-12-29 18:24:22 +0900

    メモの構造

---
+++ lib/models/note.dart
@@ -0,0 +1,31 @@
+class Note {
+  String id;
+  String title;
+  String content;
+  DateTime createdAt;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  static Note fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      content: map['content'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+}
