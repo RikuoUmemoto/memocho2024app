56d7ce020ae3f4fa79b97b444f1712ee4c8e9264
Rikuo <plumbook.menchan@gmail.com>
2025-01-07 01:29:37 +0900

    タグに対応する為にノートのモデル変更

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,14 +1,16 @@
 class Note {
-  String id;
-  String title;
-  String content;
-  DateTime createdAt;
+  final String id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+  final List<String> tags; // タグを追加
 
   Note({
     required this.id,
     required this.title,
     required this.content,
     required this.createdAt,
+    this.tags = const [],
   });
 
   Map<String, dynamic> toMap() {
@@ -17,15 +19,17 @@ class Note {
       'title': title,
       'content': content,
       'createdAt': createdAt.toIso8601String(),
+      'tags': tags, // タグを保存
     };
   }
 
   static Note fromMap(Map<String, dynamic> map) {
     return Note(
-      id: map['id'],
-      title: map['title'],
-      content: map['content'],
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      content: map['content'] ?? '',
       createdAt: DateTime.parse(map['createdAt']),
+      tags: List<String>.from(map['tags'] ?? []), // タグを復元
     );
   }
 }
