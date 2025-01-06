3027c78653cb7f8f48f5eeee5162c992dcfefbda
Rikuo <plumbook.menchan@gmail.com>
2025-01-07 01:29:48 +0900

    タグ機能

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,83 @@
-import 'package:flutter/material.dart';
-import '../models/note.dart';
 import '../backend/database_manager.dart';
 import 'note_editor.dart';
-import 'login_screen.dart';
-import '../backend/logout.dart';
+import 'package:flutter/material.dart';
+import '../models/note.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class HomeScreen extends StatelessWidget {
-  final FirebaseManager _databaseService = FirebaseManager();
-  final AuthService _authService = AuthService(); // AuthService のインスタンスを作成
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  // ログアウト処理
-  Future<void> handleLogout(BuildContext context) async {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseManager _firebaseManager = FirebaseManager();
+  List<Map<String, dynamic>> _notes = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNotes();
+  }
+
+  Future<void> _fetchNotes() async {
+    setState(
+      () {
+        _isLoading = true;
+      },
+    );
+
     try {
-      await _authService.signOut(); // AuthService の signOut メソッドを呼び出す
-      // ログアウト後、ログイン画面に戻す
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-            builder: (context) => LoginScreen()), // LoginScreen に遷移
+      // Note のリストを取得
+      final List<Note> notes = await _firebaseManager.getNotes();
+
+      // List<Note> を List<Map<String, dynamic>> に変換
+      final List<Map<String, dynamic>> notesMap =
+          notes.map((note) => note.toMap()).toList();
+
+      setState(
+        () {
+          _notes = notesMap;
+        },
       );
     } catch (e) {
-      // エラーが発生した場合にエラーメッセージを表示
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("ログアウト失敗: ${e.toString()}")),
+        SnackBar(content: Text('Failed to fetch notes: $e')),
+      );
+    } finally {
+      setState(
+        () {
+          _isLoading = false;
+        },
+      );
+    }
+  }
+
+  Future<void> _navigateToEditor({String? noteId}) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => NoteEditorScreen(noteId: noteId),
+      ),
+    );
+    _fetchNotes();
+  }
+
+  // ログアウト処理
+  Future<void> _logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      // ログアウト後の遷移など、必要な処理を追加
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Successfully logged out')),
+      );
+      // ログアウト後にホーム画面をリダイレクトする場合
+      Navigator.pushReplacementNamed(context, '/login'); // ログイン画面に遷移
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to log out: $e')),
       );
     }
   }
@@ -31,54 +86,58 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('メモ帳'),
+        title: const Text('メモ一覧'),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () =>
-                handleLogout(context), // ログアウトボタンが押されたら handleLogout を呼び出す
+            icon: const Icon(Icons.logout),
+            onPressed: _logout,
           ),
         ],
       ),
-      body: StreamBuilder<List<Note>>(
-        stream: _databaseService.getNotesStream(), // リアルタイムでデータを監視
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('メモがありません'));
-          }
-          final notes = snapshot.data!;
-          return ListView.builder(
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              final note = notes[index];
-              return ListTile(
-                title: Text(note.title),
-                subtitle: Text(note.content),
-                onTap: () {
-                  // 編集画面に遷移
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => NoteEditorScreen(noteId: note.id),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        },
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _notes.isEmpty
+              ? const Center(child: Text('No notes available'))
+              : ListView.builder(
+                  itemCount: _notes.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final Map<String, dynamic> note = _notes[index];
+                    final List<String> tags =
+                        List<String>.from(note['tags'] ?? []);
+
+                    return Card(
+                      margin: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        title: Text(note['title']),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              note['content'],
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 8.0),
+                            Wrap(
+                              spacing: 8.0,
+                              children: tags
+                                  .map<Widget>((tag) => Chip(label: Text(tag)))
+                                  .toList(),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          _navigateToEditor(noteId: note['id']);
+                        },
+                      ),
+                    );
+                  },
+                ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NoteEditorScreen()),
-          );
+          _navigateToEditor();
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
